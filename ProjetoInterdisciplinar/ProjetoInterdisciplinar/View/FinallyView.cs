a6d054cd1e896d4fd8cf47613f3a45a53d0083e1
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-11 02:53:46 -0300

    Implmentation logic to show multiple cells in the finally screen

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/FinallyView.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/FinallyView.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProjetoInterdisciplinar.View;
 
 namespace ProjetoInterdisciplinar.View
 {
@@ -15,6 +16,35 @@ namespace ProjetoInterdisciplinar.View
         public FinallyView()
         {
             InitializeComponent();
+        }
+
+        private void FinallyView_Load(object sender, EventArgs e)
+        {
+            poplateItems();
+        }
+
+        private void poplateItems()
+        {
+            ListItem[] listItems = new ListItem[20];
+
+            for (int i = 0; i < listItems.Length; i++)
+            {
+
+                listItems[i] = new ListItem();
+                listItems[i].UserName = "Pamela Dantas";
+                listItems[i].DescriptionProduct = "Refrigerante Coca Cola Pet 3L";
+                listItems[i].SupermarketName = "Supermercado Enxuto";
+                listItems[i].Price = "R$ 9.99";
+
+                if (flowLayoutPanel1.Controls.Count < 0)
+                {
+                    flowLayoutPanel1.Controls.Clear();
+                }
+                else
+                {
+                    flowLayoutPanel1.Controls.Add(listItems[i]);
+                }
+            }
         }
     }
 }
