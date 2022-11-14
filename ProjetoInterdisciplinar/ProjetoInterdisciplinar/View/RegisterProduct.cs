5d46fcd277e8a375cf506962ba519285c2495d25
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-13 23:12:32 -0300

    Create method to loadCategories i

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterProduct.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterProduct.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ProjetoInterdisciplinar.VO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,36 @@ namespace ProjetoInterdisciplinar.View
 {
     public partial class RegisterProduct : Form
     {
+        private CategoryVO crudVO;
+
         public RegisterProduct()
         {
             InitializeComponent();
+            crudVO = new CategoryVO();
         }
+
+        private void RegisterProduct_Load(object sender, EventArgs e)
+        {
+            loadCategory();
+        }
+
+        private void loadCategory()
+        {
+            try
+            {
+                DataTable dataTable = crudVO.selectCategory();
+                if (dataTable.Rows.Count > 0)
+                {
+                    cbCategory.DataSource = dataTable;
+                    cbCategory.ValueMember = dataTable.Columns[0].ColumnName;
+                    cbCategory.DisplayMember = dataTable.Columns[1].ColumnName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
