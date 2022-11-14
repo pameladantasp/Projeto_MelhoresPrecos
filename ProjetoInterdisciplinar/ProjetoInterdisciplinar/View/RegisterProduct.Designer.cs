24f90fd09b41bfeddf893dd02108a2469a085075
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-13 23:09:35 -0300

    Add new components Register product view

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterProduct.Designer.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterProduct.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.panelName = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbCategory = new System.Windows.Forms.Label();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.panelName.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            this.pctLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pctLogo.Image = global::ProjetoInterdisciplinar.Properties.Resources.logoAppTranslucidoVermelho;
+            this.pctLogo.Location = new System.Drawing.Point(149, 12);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(130, 113);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctLogo.TabIndex = 25;
+            this.pctLogo.TabStop = false;
+            // 
+            // panelName
+            // 
+            this.panelName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelName.Controls.Add(this.panel2);
+            this.panelName.Controls.Add(this.txtName);
+            this.panelName.Controls.Add(this.lbDescription);
+            this.panelName.Location = new System.Drawing.Point(36, 165);
+            this.panelName.Margin = new System.Windows.Forms.Padding(2);
+            this.panelName.Name = "panelName";
+            this.panelName.Size = new System.Drawing.Size(355, 35);
+            this.panelName.TabIndex = 26;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(16, 31);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(330, 1);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.BackColor = System.Drawing.Color.White;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(93, 8);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(253, 16);
+            this.txtName.TabIndex = 0;
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescription.Location = new System.Drawing.Point(14, 9);
+            this.lbDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(75, 15);
+            this.lbDescription.TabIndex = 0;
+            this.lbDescription.Text = "Descrição:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbCategory);
+            this.panel1.Controls.Add(this.lbCategory);
+            this.panel1.Location = new System.Drawing.Point(36, 216);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 35);
+            this.panel1.TabIndex = 27;
+            // 
+            // lbCategory
+            // 
+            this.lbCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbCategory.AutoSize = true;
+            this.lbCategory.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lbCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCategory.Location = new System.Drawing.Point(13, 11);
+            this.lbCategory.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbCategory.Name = "lbCategory";
+            this.lbCategory.Size = new System.Drawing.Size(73, 15);
+            this.lbCategory.TabIndex = 1;
+            this.lbCategory.Text = "Categoria:";
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(92, 11);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cbCategory.TabIndex = 2;
+            // 
+            // RegisterProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "RegisterProduct";
+            this.ClientSize = new System.Drawing.Size(433, 531);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelName);
+            this.Controls.Add(this.pctLogo);
+            this.MaximumSize = new System.Drawing.Size(449, 570);
+            this.MinimumSize = new System.Drawing.Size(449, 570);
+            this.Name = "RegisterProduct";
+            this.Load += new System.EventHandler(this.RegisterProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.panelName.ResumeLayout(false);
+            this.panelName.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Panel panelName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbCategory;
+        private System.Windows.Forms.ComboBox cbCategory;
     }
 }
