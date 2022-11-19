e399ec1013289a6604757fa2876a78fe704aaaa3
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-19 20:26:57 -0300

    Refactoring MainView (PrincipalView)

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/Controller/MainController.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/Controller/MainController.cs
@@ -1,14 +1,86 @@
-﻿using System;
+﻿using ProjetoInterdisciplinar.View;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProjetoInterdisciplinar.Controller
 {
-    internal class MainController
+    internal class MainController: Form
     {
+        Thread t1;
 
+        public void navigateToRegisterUserView()
+        {
+            setThread(new Thread(openRegisterUserScreen));
+        }
 
+        public void navigateToLoginView()
+        {
+            setThread(new Thread(openLoginScreen));
+        }
+
+        private void setThread(Thread thread)
+        {
+            this.Close();
+            t1 = thread;
+            t1.SetApartmentState(ApartmentState.STA);
+            t1.Start();
+        }
+
+        private void openRegisterUserScreen(object obj)
+        {
+            Application.Run(new RegisterUserView());
+        }
+
+        private void openLoginScreen(object obj)
+        {
+            Application.Run(new LoginView());
+        }
+
+        public void setPlaceholder(TextBox txtSearch)
+        {
+            if (txtSearch.Text == "Busque por um produto...")
+            {
+                txtSearch.Text = "";
+                txtSearch.ForeColor = Color.Black;
+            }
+        }
+
+        public void removePlaceholder(TextBox txtSearch)
+        {
+            if (txtSearch.Text == "")
+            {
+                txtSearch.Text = "Busque por um produto...";
+                txtSearch.ForeColor = Color.Silver;
+            }
+        }
+
+        public void populateItems(FlowLayoutPanel flowPanel)
+        {
+            ListItem[] listItems = new ListItem[20];
+
+            for (int i = 0; i < listItems.Length; i++)
+            {
+                listItems[i] = new ListItem();
+                listItems[i].UserName = "Pamela Dantas";
+                listItems[i].DescriptionProduct = "Refrigerante Coca Cola Pet 3L";
+                listItems[i].SupermarketName = "Supermercado Enxuto";
+                listItems[i].Price = "R$ 9.99";
+
+                if (flowPanel.Controls.Count < 0)
+                {
+                    flowPanel.Controls.Clear();
+                }
+                else
+                {
+                    flowPanel.Controls.Add(listItems[i]);
+                }
+            }
+        }
     }
 }
--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/MainView.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/MainView.cs
@@ -2,90 +2,43 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Threading;
+using ProjetoInterdisciplinar.Controller;
 
 namespace ProjetoInterdisciplinar.View
 {
     public partial class MainView : Form
     {
-        Thread t2;
-        Thread t3;
+       private MainController mainController;
+
         public MainView()
         {
             InitializeComponent();
+            mainController = new MainController();
         }
 
         private void lbLogin_Click(object sender, EventArgs e)
         {
-            this.Close();
-            t2 = new Thread(openLoginView);
-            t2.SetApartmentState(ApartmentState.STA);
-            t2.Start();
-        }
-
-        private void openLoginView(object obj)
-        {
-            Application.Run(new LoginView());
+            mainController.navigateToLoginView();
         }
 
         private void lbRegister_Click(object sender, EventArgs e)
         {
-            this.Close();
-            t3 = new Thread(openRegisterUserView);
-            t3.SetApartmentState(ApartmentState.STA);
-            t3.Start();
-        }
-
-        private void openRegisterUserView(object obj)
-        {
-            Application.Run(new RegisterUserView());
+            mainController.navigateToRegisterUserView();
         }
 
         private void txtSearch_Enter(object sender, EventArgs e)
         {
-            if(txtSearch.Text == "Busque por um produto...")
-            {
-                txtSearch.Text = "";
-                txtSearch.ForeColor = Color.Black;
-            }
+            mainController.setPlaceholder(txtSearch);
         }
 
         private void txtSearch_Leave(object sender, EventArgs e)
         {
-            if( txtSearch.Text == "")
-            {
-                txtSearch.Text = "Busque por um produto...";
-                txtSearch.ForeColor = Color.Silver;
-            }
+            mainController.removePlaceholder(txtSearch);
         }
 
-        private void PrincipalView_Load(object sender, EventArgs e)
+        private void MainView_Load(object sender, EventArgs e)
         {
-            poplateItems();
-        }
-
-        private void poplateItems()
-        {
-            ListItem[] listItems = new ListItem[20];
-
-            for (int i = 0; i < listItems.Length; i++) {
-
-                listItems[i] = new ListItem();
-                listItems[i].UserName = "Pamela Dantas";
-                listItems[i].DescriptionProduct = "Refrigerante Coca Cola Pet 3L";
-                listItems[i].SupermarketName = "Supermercado Enxuto";
-                listItems[i].Price = "R$ 9.99";
-
-                if (flowLayoutPanel1.Controls.Count < 0)
-                {
-                    flowLayoutPanel1.Controls.Clear();
-                }
-                else
-                {
-
-                    flowLayoutPanel1.Controls.Add(listItems[i]);
-                }
-
-            }
+            mainController.populateItems(flowLayoutPanel1);
         }
     }
 }
