9389a3eb87de26221cc4d7ff84cca4cd531716a8
pamela dantas <112967736+pameladantasp@users.noreply.github.com>
2022-11-19 11:45:22 -0300

    Update query_DeleteCustomerID.sql

--- query_DeleteCustomerID.sql
+++ query_DeleteCustomerID.sql
@@ -1,6 +1,7 @@
-SELECT * FROM sql10527490.customer;
-
 DELETE FROM customer WHERE idCustomer = (SELECT idCustomer FROM (SELECT idCustomer FROM customer
-									WHERE email = 'pamela@gmail.com') AS GetCustomerID)
+									WHERE email = 'pamela@gmail.com') AS DeleteCustomerID)
 ;
 
+# no código
+DELETE FROM customer WHERE idCustomer = (SELECT idCustomer FROM (SELECT idCustomer FROM customer
+									WHERE email = @email) AS DeleteCustomerID)
