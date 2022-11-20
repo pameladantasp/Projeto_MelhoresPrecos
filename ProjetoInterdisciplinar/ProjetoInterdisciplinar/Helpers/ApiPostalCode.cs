08ccd8a05bc6806e64bb1156a5285963921cece5
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-20 20:32:03 -0300

    Remove imports

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/Helpers/ApiPostalCode.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/Helpers/ApiPostalCode.cs
@@ -1,10 +1,6 @@
 ﻿using Correios;
 using ProjetoInterdisciplinar.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProjetoInterdisciplinar.Helpers
