aacd547568753c2fcbd1f23546164a797cb0421c
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-19 21:21:42 -0300

    Refactoring RegisterUserView

---
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/Controller/RegisterUserController.cs
@@ -0,0 +1,95 @@
+﻿using Correios;
+using ProjetoInterdisciplinar.Model;
+using ProjetoInterdisciplinar.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ProjetoInterdisciplinar.Controller
+{
+    internal class RegisterUserController: Form
+    {
+        Thread t1;
+
+        public void navigateToMainView()
+        {
+            setThread(new Thread(openMainScreen));
+        }
+
+        public void navigateToHomeView()
+        {
+            setThread(new Thread(openHomeScreen));
+        }
+
+        private void setThread(Thread thread)
+        {
+            this.Close();
+            t1 = thread;
+            t1.SetApartmentState(ApartmentState.STA);
+            t1.Start();
+        }
+
+        private void openMainScreen(object obj)
+        {
+            Application.Run(new MainView());
+        }
+
+        private void openHomeScreen(object obj)
+        {
+            Application.Run(new HomeView());
+        }
+
+        public void localizePostalCode(
+            MaskedTextBox txtPostalCode,
+            TextBox txtState,
+            TextBox txtStreet,
+            TextBox txtCity
+        )
+        {
+            if (string.IsNullOrEmpty(txtPostalCode.Text))
+            {
+                MessageBox.Show("O campo CEP esta vazio", "Atencao!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                try
+                {
+                    CorreiosApi correiosApi = new CorreiosApi();
+                    var response = correiosApi.consultaCEP(txtPostalCode.Text);
+
+                    if (response is null)
+                    {
+                        MessageBox.Show("CEP nao encontrado", "Atencao!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+
+                    txtState.Text = response.uf;
+                    txtStreet.Text = response.end;
+                    txtCity.Text = response.cidade;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show($"{error.Message}");
+                }
+            }
+        }
+
+        public void userSignUp(Customer customer)
+        {
+            try
+            {
+                customer.insertCostumer();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao realizar a operação", "Database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterUserView.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RegisterUserView.cs
@@ -2,106 +2,47 @@
 using System;
 using System.Windows.Forms;
 using System.Threading;
-
+using ProjetoInterdisciplinar.Controller;
+using ProjetoInterdisciplinar.Model;
 
 namespace ProjetoInterdisciplinar.View
 {
     public partial class RegisterUserView : Form
     {
-        Thread thread;
-        private CustomerVO crudVO;
+        private RegisterUserController registerUserController;
 
         public RegisterUserView()
         {
             InitializeComponent();
-            crudVO = new CustomerVO();
+            registerUserController = new RegisterUserController();
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            try
-            {
-                crudVO.customer.name = txtName.Text.Trim();
-                crudVO.customer.email = txtEmail.Text.Trim();
-                crudVO.customer.password = txtPassword.Text.Trim();
+            Customer customer = new Customer();
+            customer.name = txtName.Text.Trim();
+            customer.email = txtEmail.Text.Trim();
+            customer.password = txtPassword.Text.Trim();
 
-                crudVO.customer.address.street = txtStreet.Text;
-                crudVO.customer.address.number = txtNumber.Text;
-                crudVO.customer.address.city = txtCity.Text;
-                crudVO.customer.address.state = txtState.Text;
-                crudVO.customer.address.postalCode = txtPostalCode.Text;
+            customer.address = new Address();
+            customer.address.street = txtStreet.Text;
+            customer.address.number = txtNumber.Text;
+            customer.address.city = txtCity.Text;
+            customer.address.state = txtState.Text;
+            customer.address.postalCode = txtPostalCode.Text;
 
-                crudVO.insertCostumer();
-                MessageBox.Show("DEU CERTO INSERT!");
-
-                this.Close();
-                thread = new Thread(openFinllayScreen);
-                thread.SetApartmentState(ApartmentState.STA);
-                thread.Start();
-
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ocorreu um erro ao realizar a operação", "Database", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            registerUserController.userSignUp(customer);
+            registerUserController.navigateToHomeView();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            this.Close();
-            thread = new Thread(openPrincipalScreen);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
+            registerUserController.navigateToMainView();
         }
 
         private void txtPostalCode_Leave(object sender, EventArgs e)
         {
-            callPostalCodeService();
-        }
-
-        private void callPostalCodeService()
-        {
-            localizePostalCode();
-        }
-
-            private void localizePostalCode()
-        {
-            if (string.IsNullOrEmpty(txtPostalCode.Text))
-            {
-                MessageBox.Show("O campo CEP esta vazio", "Atencao!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
-            else
-            {
-                try
-                {
-                    CorreiosApi correiosApi = new CorreiosApi();
-                    var response = correiosApi.consultaCEP(txtPostalCode.Text);
-
-                    if (response is null)
-                    {
-                        MessageBox.Show("CEP nao encontrado", "Atencao!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                        return;
-                    }
-
-                    txtState.Text = response.uf;
-                    txtStreet.Text = response.end;
-                    txtCity.Text = response.cidade;
-                }
-                catch (Exception error)
-                {
-                    MessageBox.Show($"{error.Message}");
-                }
-            }
-        }
-
-        private void openFinllayScreen(object obj)
-        {
-            Application.Run(new HomeView());
-        }
-
-        private void openPrincipalScreen(object obj)
-        {
-            Application.Run(new MainView());
+            registerUserController.localizePostalCode(txtPostalCode, txtState, txtStreet, txtCity);
         }
     }
 }
