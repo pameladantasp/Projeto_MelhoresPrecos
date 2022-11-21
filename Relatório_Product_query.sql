eceb3c1615a9c35746653221e85278ce85b586e7
pamela dantas <112967736+pameladantasp@users.noreply.github.com>
2022-11-20 23:03:12 -0300

    Gerar_Relatório_Produto
    Relatório de todos produtos cadastrados pelo usuário

---
+++ Relatório_Product_query.sql
@@ -0,0 +1,21 @@
+SELECT 
+    C.name 'Customer',
+    S.name 'Supermarket',
+    P.name 'Product',
+    R.price 'Price',
+    R.dateRegister 'Date Register'
+FROM
+    registerProduct R
+        INNER JOIN
+    supermarket S ON R.idSupermarket = S.idSupermarket
+        INNER JOIN
+    product P ON R.idProduct = P.idProduct
+        INNER JOIN
+    customer C ON R.idCustomer = C.idCustomer
+WHERE
+    C.idCustomer IN (SELECT idCustomer FROM
+					(SELECT idCustomer FROM customer WHERE email = 'testeUser@gmail.com') 
+					  AS CustomerIDRelatory)
+ORDER BY R.dateRegister DESC
+
+## Substituir o email = 'testeUser@gmail.com' para email = @email
