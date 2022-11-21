dc5e8f21e865b6fc5921ec6d5bed77243dddea13
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-20 22:56:06 -0300

    Created event Load data to listItem

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/ListItem.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/ListItem.cs
@@ -4,16 +4,23 @@ namespace ProjetoInterdisciplinar.View
 {
     public partial class ListItem : UserControl
     {
-        public ListItem()
-        {
-            InitializeComponent();
-        }
-
-
         public string UserName { get; set; }
         public string DescriptionProduct { get; set; }
         public string SupermarketName { get; set; }
         public string Price { get; set; }
 
+        public ListItem()
+        {
+            InitializeComponent();
+        }
+
+        private void ListItem_Load(object sender, System.EventArgs e)
+        {
+            lbUserName.Text = UserName;
+            lbDescriptionProduct.Text = DescriptionProduct;
+            lbNameSupermarket.Text = SupermarketName;
+            lbPrice.Text = Price;
+            lbPrice.Text = Price;
+        }
     }
 }
