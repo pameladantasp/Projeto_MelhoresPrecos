5b748fc82b8c4928019141551e3d60937f421a97
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-20 22:57:54 -0300

    Refactoring HomeView

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/HomeView.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/HomeView.cs
@@ -1,70 +1,32 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Threading;
+using ProjetoInterdisciplinar.Controller;
 
 namespace ProjetoInterdisciplinar.View
 {
     public partial class HomeView : Form
     {
-        Thread thread;
+        private HomeController homeController;
         public HomeView()
         {
             InitializeComponent();
         }
 
-        private void FinallyView_Load(object sender, EventArgs e)
+        private void HomeView_Load(object sender, EventArgs e)
         {
-            poplateItems();
-        }
-
-        private void poplateItems()
-        {
-            ListItem[] listItems = new ListItem[20];
-
-            for (int i = 0; i < listItems.Length; i++)
-            {
-
-                listItems[i] = new ListItem();
-                listItems[i].UserName = "Pamela Dantas";
-                listItems[i].DescriptionProduct = "Refrigerante Coca Cola Pet 3L";
-                listItems[i].SupermarketName = "Supermercado Enxuto";
-                listItems[i].Price = "R$ 9.99";
-
-                if (flowLayoutPanel1.Controls.Count < 0)
-                {
-                    flowLayoutPanel1.Controls.Clear();
-                }
-                else
-                {
-                    flowLayoutPanel1.Controls.Add(listItems[i]);
-                }
-            }
+            homeController.populateItems(flowLayoutPanel1);
         }
 
         private void btnLogout_Click(object sender, EventArgs e)
         {
-            this.Close();
-            thread = new Thread(openPrincipalScreen);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-        }
-
-        private void openPrincipalScreen(object obj)
-        {
-            Application.Run(new MainView());
+            homeController.closeView(this);
+            homeController.navigateToMainView();
         }
 
         private void btnRegisterProduct_Click(object sender, EventArgs e)
         {
-            this.Close();
-            thread= new Thread(openRegisterProductScreen);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-        }
-
-        private void openRegisterProductScreen()
-        {
-            Application.Run(new RegisterProductView());
+            homeController.closeView(this);
+            homeController.navigateToRegisterProductView();
         }
     }
 }
