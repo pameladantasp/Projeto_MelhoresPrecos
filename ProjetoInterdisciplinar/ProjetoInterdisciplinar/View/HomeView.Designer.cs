b744fec3894318ba2a335fe703a35e591932e229
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-27 20:53:30 -0300

    Update desing HomeView

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/HomeView.Designer.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/HomeView.Designer.cs
@@ -37,12 +37,12 @@
             this.btnRegisterProduct = new System.Windows.Forms.Button();
             this.panelUserInfo = new System.Windows.Forms.Panel();
             this.pctUser = new System.Windows.Forms.PictureBox();
-            this.lbUserName = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panelSearch = new System.Windows.Forms.Panel();
             this.labelSearch = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lbUserName = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panelUserInfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctUser)).BeginInit();
@@ -73,9 +73,9 @@
             this.btnConfiguration.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConfiguration.ForeColor = System.Drawing.Color.White;
             this.btnConfiguration.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_configuration_28;
-            this.btnConfiguration.Location = new System.Drawing.Point(0, 298);
+            this.btnConfiguration.Location = new System.Drawing.Point(3, 307);
             this.btnConfiguration.Name = "btnConfiguration";
-            this.btnConfiguration.Size = new System.Drawing.Size(176, 48);
+            this.btnConfiguration.Size = new System.Drawing.Size(170, 48);
             this.btnConfiguration.TabIndex = 2;
             this.btnConfiguration.Text = "Configurações";
             this.btnConfiguration.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -105,9 +105,9 @@
             this.button3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_ranking28;
-            this.button3.Location = new System.Drawing.Point(0, 253);
+            this.button3.Location = new System.Drawing.Point(3, 262);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(176, 48);
+            this.button3.Size = new System.Drawing.Size(170, 48);
             this.button3.TabIndex = 1;
             this.button3.Text = "Ranking de Supermercados";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -120,9 +120,9 @@
             this.btnHistoryPrice.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHistoryPrice.ForeColor = System.Drawing.Color.White;
             this.btnHistoryPrice.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_history_28;
-            this.btnHistoryPrice.Location = new System.Drawing.Point(0, 209);
+            this.btnHistoryPrice.Location = new System.Drawing.Point(3, 216);
             this.btnHistoryPrice.Name = "btnHistoryPrice";
-            this.btnHistoryPrice.Size = new System.Drawing.Size(176, 48);
+            this.btnHistoryPrice.Size = new System.Drawing.Size(170, 48);
             this.btnHistoryPrice.TabIndex = 1;
             this.btnHistoryPrice.Text = "Historico de preços";
             this.btnHistoryPrice.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -135,9 +135,9 @@
             this.btnReport.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReport.ForeColor = System.Drawing.Color.White;
             this.btnReport.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_add;
-            this.btnReport.Location = new System.Drawing.Point(0, 165);
+            this.btnReport.Location = new System.Drawing.Point(3, 172);
             this.btnReport.Name = "btnReport";
-            this.btnReport.Size = new System.Drawing.Size(176, 48);
+            this.btnReport.Size = new System.Drawing.Size(170, 48);
             this.btnReport.TabIndex = 1;
             this.btnReport.Text = "Gerar Relatório    ";
             this.btnReport.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -152,9 +152,9 @@
             this.btnRegisterProduct.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegisterProduct.ForeColor = System.Drawing.Color.White;
             this.btnRegisterProduct.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_add_28;
-            this.btnRegisterProduct.Location = new System.Drawing.Point(0, 121);
+            this.btnRegisterProduct.Location = new System.Drawing.Point(3, 127);
             this.btnRegisterProduct.Name = "btnRegisterProduct";
-            this.btnRegisterProduct.Size = new System.Drawing.Size(176, 48);
+            this.btnRegisterProduct.Size = new System.Drawing.Size(170, 48);
             this.btnRegisterProduct.TabIndex = 1;
             this.btnRegisterProduct.Text = "Adicionar Produto";
             this.btnRegisterProduct.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -166,36 +166,22 @@
             this.panelUserInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panelUserInfo.Controls.Add(this.pctUser);
             this.panelUserInfo.Controls.Add(this.lbUserName);
+            this.panelUserInfo.Controls.Add(this.pctUser);
             this.panelUserInfo.Location = new System.Drawing.Point(0, 0);
             this.panelUserInfo.Name = "panelUserInfo";
-            this.panelUserInfo.Size = new System.Drawing.Size(176, 115);
+            this.panelUserInfo.Size = new System.Drawing.Size(173, 121);
             this.panelUserInfo.TabIndex = 0;
             // 
             // pctUser
             // 
             this.pctUser.Image = global::ProjetoInterdisciplinar.Properties.Resources.icon_user_white_100;
-            this.pctUser.Location = new System.Drawing.Point(38, 12);
+            this.pctUser.Location = new System.Drawing.Point(37, 15);
             this.pctUser.Name = "pctUser";
             this.pctUser.Size = new System.Drawing.Size(100, 66);
             this.pctUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pctUser.TabIndex = 2;
             this.pctUser.TabStop = false;
-            // 
-            // lbUserName
-            // 
-            this.lbUserName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbUserName.AutoSize = true;
-            this.lbUserName.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbUserName.ForeColor = System.Drawing.Color.White;
-            this.lbUserName.Location = new System.Drawing.Point(44, 81);
-            this.lbUserName.Name = "lbUserName";
-            this.lbUserName.Size = new System.Drawing.Size(94, 21);
-            this.lbUserName.TabIndex = 1;
-            this.lbUserName.Text = "User Name";
             // 
             // flowLayoutPanel1
             // 
@@ -262,6 +248,22 @@
             this.txtSearch.Enter += new System.EventHandler(this.txtSearch_Enter);
             this.txtSearch.Leave += new System.EventHandler(this.txtSearch_Leave);
             // 
+            // lbUserName
+            // 
+            this.lbUserName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUserName.ForeColor = System.Drawing.Color.White;
+            this.lbUserName.Location = new System.Drawing.Point(12, 84);
+            this.lbUserName.MaximumSize = new System.Drawing.Size(176, 0);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.lbUserName.Size = new System.Drawing.Size(100, 21);
+            this.lbUserName.TabIndex = 1;
+            this.lbUserName.Text = "User Name";
+            // 
             // HomeView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -292,7 +294,6 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panelUserInfo;
-        private System.Windows.Forms.Label lbUserName;
         private System.Windows.Forms.Button btnRegisterProduct;
         private System.Windows.Forms.PictureBox pctUser;
         private System.Windows.Forms.Button btnLogout;
@@ -305,5 +306,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.Button btnConfiguration;
+        private System.Windows.Forms.Label lbUserName;
     }
 }
