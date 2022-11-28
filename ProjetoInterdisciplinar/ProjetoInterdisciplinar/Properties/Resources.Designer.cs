fd4635c3cd3b7a2b3e5154ad34e9bffd65d57d1d
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-28 18:00:45 -0300

    Add icons user

--- ProjetoInterdisciplinar/ProjetoInterdisciplinar/Properties/Resources.Designer.cs
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/Properties/Resources.Designer.cs
@@ -143,9 +143,28 @@ namespace ProjetoInterdisciplinar.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_user_100 {
+            get {
+                object obj = ResourceManager.GetObject("icon-user-100", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon_user_28 {
             get {
                 object obj = ResourceManager.GetObject("icon-user-28", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_user_50 {
+                object obj = ResourceManager.GetObject("icon-user-50", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
