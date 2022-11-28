7c94c28d15e98ac57609782f80f6ed9fa8df843e
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-11-27 21:10:21 -0300

    Create RetrievePasswordView

---
+++ ProjetoInterdisciplinar/ProjetoInterdisciplinar/View/RetrievePassword.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ProjetoInterdisciplinar.View
+{
+    partial class RetrievePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panelBtnRegister = new System.Windows.Forms.Panel();
+            this.btnRetrievePassword = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelEmail = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panelBtnRegister.SuspendLayout();
+            this.panelEmail.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(52, 391);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 44);
+            this.panel1.TabIndex = 36;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(82)))), ((int)(((byte)(65)))));
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(16, 2);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(309, 40);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "VOLTAR";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // panelBtnRegister
+            // 
+            this.panelBtnRegister.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelBtnRegister.Controls.Add(this.btnRetrievePassword);
+            this.panelBtnRegister.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panelBtnRegister.Location = new System.Drawing.Point(52, 343);
+            this.panelBtnRegister.Margin = new System.Windows.Forms.Padding(2);
+            this.panelBtnRegister.Name = "panelBtnRegister";
+            this.panelBtnRegister.Size = new System.Drawing.Size(334, 44);
+            this.panelBtnRegister.TabIndex = 35;
+            // 
+            // btnRetrievePassword
+            // 
+            this.btnRetrievePassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRetrievePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(82)))), ((int)(((byte)(65)))));
+            this.btnRetrievePassword.FlatAppearance.BorderSize = 0;
+            this.btnRetrievePassword.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetrievePassword.ForeColor = System.Drawing.Color.White;
+            this.btnRetrievePassword.Location = new System.Drawing.Point(16, 2);
+            this.btnRetrievePassword.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRetrievePassword.Name = "btnRetrievePassword";
+            this.btnRetrievePassword.Size = new System.Drawing.Size(309, 40);
+            this.btnRetrievePassword.TabIndex = 3;
+            this.btnRetrievePassword.Text = "REDEFINIR";
+            this.btnRetrievePassword.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 33);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Redefinir Senha";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelEmail
+            // 
+            this.panelEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelEmail.Controls.Add(this.panel4);
+            this.panelEmail.Controls.Add(this.txtEmail);
+            this.panelEmail.Controls.Add(this.lblEmail);
+            this.panelEmail.Location = new System.Drawing.Point(52, 249);
+            this.panelEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.panelEmail.Name = "panelEmail";
+            this.panelEmail.Size = new System.Drawing.Size(334, 35);
+            this.panelEmail.TabIndex = 32;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(16, 31);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(309, 1);
+            this.panel4.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmail.BackColor = System.Drawing.Color.White;
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(62, 8);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(263, 16);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(13, 9);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 15);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Email:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoInterdisciplinar.Properties.Resources.logoAppTranslucidoVermelho;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RetrievePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(438, 555);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelBtnRegister);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelEmail);
+            this.MaximumSize = new System.Drawing.Size(454, 594);
+            this.MinimumSize = new System.Drawing.Size(454, 594);
+            this.Name = "RetrievePassword";
+            this.panel1.ResumeLayout(false);
+            this.panelBtnRegister.ResumeLayout(false);
+            this.panelEmail.ResumeLayout(false);
+            this.panelEmail.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel panelBtnRegister;
+        private System.Windows.Forms.Button btnRetrievePassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelEmail;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
